66bd70be3814f0e267143959605271889cd9c900
junbob <junbob@sfu.ca>
2016-11-15 02:20:19 -0800

    update google map in create

--- CodeIgniter/application/views/activity/create.php
+++ CodeIgniter/application/views/activity/create.php
@@ -4,26 +4,32 @@
 
 <?php echo validation_errors(); ?>
 <?php echo form_open("activity/create/$location/$user_id"); ?>
-      <lable "activity name">Activity name: <lable><br />
+      <lable "activity name">Activity name: </lable><br />
       <input type="input" name="name" /><br />
-      <lable "activity date">Activity date: <lable><br />
+      <lable "activity date">Activity date: </lable><br />
       <input type="input" name="date" /><br />
-      <lable "activity time">Activity time: <lable><br />
+      <lable "activity time">Activity time: </lable><br />
       <input type="input" name="time" /><br />
-      <lable "catagory">Catagory: <lable><br />
+      <lable "catagory">Catagory: </lable><br />
       <input type="input" name="catagory" /><br />
-      <lable "location_lng">Location lng: <lable><br />
+      <lable "location_lng">Location lng: </lable><br />
       <input  type="input" id="location" name="location_lng" /><br />
-      <lable "location_lat">Location lat: <lable><br />
-      <input type="input" name="location_lat" /><lable><br />
-      <lable "description">Description: <lable><br />
+      <lable "location_lat">Location lat: </lable><br />
+      <input type="input" name="location_lat" /></lable><br />
+
+      <lable "placeBox">Address: </lable><br />
+      <input type="text" id="myPlaceTextBox" /><br />
+
+      <lable "description">Description: </lable><br />
       <textarea cols="40" rows="10" name="description"></textarea><br />
 
       <input type="submit" name="submit" value="Create an activity" >
 </form>
 
 <br><br><br>
-<input type="text" id="myPlaceTextBox" />
+
+
+<!--
 <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function()
@@ -38,5 +44,5 @@ $(document).ready(function()
   $('#myPlaceTextBox').on('mouseenter',change_location);
 });
 </script>
-
+-->
  <?php echo $map['html']; ?>
