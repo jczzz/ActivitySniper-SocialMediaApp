8167c8e17bbd06abb782cf6e1fe2a5bc13fe7032
junbob <junbob@sfu.ca>
2016-11-16 22:32:01 -0800

    update db tables

--- mysql/initial_act.sql
+++ mysql/initial_act.sql
@@ -1,8 +1,8 @@
-INSERT INTO activity VALUES(NULL,'1','basketball','2016/11/14','01:10:00','basketball is a very nice basketball','-123.116226','49.246292','outdoor');
-INSERT INTO activity VALUES(NULL,'1','tennis','2016/12/15','13:00:00','tennis is a very nice tennis','-123.133568','49.166592','outdoor');
-INSERT INTO activity VALUES(NULL,'1','party','2016/12/20','15:00:00','party is a very nice party','-122.793226','49.283892','indoor');
-INSERT INTO activity VALUES(NULL,'1','dinner','2016/11/14','00:50:00','dinner is a very nice dinner','-123.073326','49.327092','indoor');
-INSERT INTO activity VALUES(NULL,'1','football','2016/11/14','00:50:00','dinner is a very nice dinner','-123.078326','49.327092','indoor');
+INSERT INTO activity VALUES(NULL,'1','basketball','2016/11/14','01:10:00','basketball is a very nice basketball','2901 E Hastings St, Vancouver, BC','outdoor');
+INSERT INTO activity VALUES(NULL,'1','tennis','2016/12/15','13:00:00','tennis is a very nice tennis','999 Canada Pl, Vancouver, BC','outdoor');
+INSERT INTO activity VALUES(NULL,'1','party','2016/12/20','15:00:00','party is a very nice party','350 W Georgia St, Vancouver, BC','indoor');
+INSERT INTO activity VALUES(NULL,'1','dinner','2016/11/14','00:50:00','dinner is a very nice dinner','1669 Johnston St, Vancouver, BC','indoor');
+INSERT INTO activity VALUES(NULL,'1','football','2016/11/14','00:50:00','dinner is a very nice dinner','2329 West Mall, Vancouver, BC','indoor');
 
 
 
--- mysql/user.sql
+++ mysql/user.sql
@@ -1,12 +1,13 @@
 CREATE TABLE users (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        firstname varchar(128) NOT NULL,
-        lastname varchar(128) NOT NULL,
-  		phonenum varchar(128),
-      	email varchar(128) NOT NULL,
-		password varchar(256) NOT NULL,
-		notes text,
-        PRIMARY KEY (id)
+  id int(11) NOT NULL AUTO_INCREMENT,
+  firstname varchar(128) NOT NULL,
+  lastname varchar(128) NOT NULL,
+  phonenum varchar(128),
+  email varchar(128) NOT NULL,
+  password varchar(256) NOT NULL,
+  notes text,
+  picture varchar(128),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO users VALUES(1,'admin','admin','7783166613','zgeng@sfu.ca','14789632','I am the admin');
+INSERT INTO users VALUES(1,'admin','admin','7783166613','zgeng@sfu.ca','14789632','I am the admin','');
