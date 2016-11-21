f86c73b5fdf59a3a724a38b182cdb6530511107a
junbob <junbob@sfu.ca>
2016-11-21 00:13:25 -0800

    remove unused files

--- CodeIgniter/application/views/user/#friendlist#.php
+++
@@ -1,6 +0,0 @@
-<?php foreach ($result as $friend_item): ?>
-<a href="<?php echo site_url("user/information/".$friend_item['id']."/")?>"><?php echo $friend_item['email']; ?></a>
-<br /><br />
-<?php endforeach; ?>
-
-<a href="<?php echo site_url("activity/index/SFU/")?>">Back to your activities</a>
